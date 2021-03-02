bd1bd8618a224685d8799c2039c15311ee838646
Dillon Shaffer <d.j.shaffer@outlook.com>
2021-03-01 18:24:45 -0600

    Migrate to null-safety
    This commit migrates the library, its test, and its example to support null safety

--- example/main.dart
+++ example/main.dart
@@ -5,8 +5,8 @@ import 'package:xxtea/xxtea.dart';
 void main() {
   String str = "Hello World! 你好，中国🇨🇳！";
   String key = "1234567890";
-  String encrypt_data = xxtea.encryptToString(str, key);
+  String? encrypt_data = xxtea.encryptToString(str, key);
   print(encrypt_data);
-  String decrypt_data = xxtea.decryptToString(encrypt_data, key);
+  String? decrypt_data = xxtea.decryptToString(encrypt_data, key);
   print(decrypt_data);
 }
